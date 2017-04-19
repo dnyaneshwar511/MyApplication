dbe91be6b6affb2bce900d55468adfddd8953969
Dnyaneshwar Dalvi <dnyaneshwar.dalvi@interactive.in>
2017-04-19 12:25:37 +0530

    Initial commit

---
+++ app/src/main/java/imt/com/in/mygithubapp/OneMain.java
@@ -0,0 +1,15 @@
+package imt.com.in.mygithubapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OneMain extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one_main);
+    }
+}
