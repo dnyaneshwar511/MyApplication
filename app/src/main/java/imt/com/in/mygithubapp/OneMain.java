455fb0b492f94987ef087275e3867663bf3fda2d
Dnyaneshwar Dalvi <dnyaneshwar.dalvi@interactive.in>
2017-04-19 13:25:29 +0530

    Toast Changes

--- app/src/main/java/imt/com/in/mygithubapp/OneMain.java
+++ app/src/main/java/imt/com/in/mygithubapp/OneMain.java
@@ -2,6 +2,7 @@ package imt.com.in.mygithubapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class OneMain extends AppCompatActivity
 {
@@ -11,5 +12,7 @@ public class OneMain extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one_main);
+
+        Toast.makeText(OneMain.this, "Ok", Toast.LENGTH_SHORT).show();
     }
 }
